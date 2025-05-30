910a339d46bcb31cacfa9f6e1345ae458526e076
leonardojulius <leonardojulius.jcl@gmail.com>
2025-05-30 23:58:01 +0800

    Delete Feedback Elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,38 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import FeedbackList from "./components/FeedbackList.svelte";
 
-  let firstName = 'Julius';
-  let lastName ='Leonardo';
-  let color = 'blue';
-  let showText = false;
-  let users = [
-      {
-        id: 1,
-        name:'Julius',
-      },
+let feedback = [
 
-      {
-        id: 2,
-        name:'Faith',
-      },
+{
+  id: 1,
+  rating: 10,
+  text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia voluptas veniam eum ad. Praesentium, itaque? Aliquam perferendis asperiores vitae laborum, esse deserunt, illum ipsam animi molestias id dicta sapiente aperiam.',
+},
 
-      {
-        id: 3,
-        name:'Jing',
-      }
+{
+  id: 2,
+  rating: 9,
+  text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia voluptas veniam eum ad. Praesentium, itaque? Aliquam perferendis asperiores vitae laborum, esse deserunt, illum ipsam animi molestias id dicta sapiente aperiam.',
+},
+
+{
+  id: 3,
+  rating: 8,
+  text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia voluptas veniam eum ad. Praesentium, itaque? Aliquam perferendis asperiores vitae laborum, esse deserunt, illum ipsam animi molestias id dicta sapiente aperiam.',
+}
 
 
-  ];
+]
 
-  //Reactive Value;
-  $: name = firstName + ' ' + lastName;
-
-  const toggle = () => {
-    color = color === 'blue' ? 'red' : 'blue'
-    showText = !showText
-    users = [...users,{id: 4 ,name:'Jen'}]
-  }
+//Action Delete
+const deleteFeedback = (e) =>{
+   const itemId = e.detail
+   feedback = feedback.filter((item)=> item.id != itemId)
+}
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1 style="color: {color};">{name}</h1>
+<main class="container">
+   <FeedbackList {feedback} on:delete-feedback ={deleteFeedback} />
 
-  {#if showText}
-  
-    <div class="card">
-      <Counter />
-    </div>
-    <p>
-      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-    </p>
-    <p class="read-the-docs">
-      Click on the Vite and Svelte logos to learn more
-    </p>
-  {:else}
-    <p>No Text</p>
-  {/if}
-
-  <button on:click={toggle}>Click</button>
-  {#each users as user (user.id)}
-   <h3>{user.id}:{user.name}</h3>
-  {/each}
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
